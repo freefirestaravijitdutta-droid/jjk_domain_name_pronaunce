27817ee7c797c8f57e6f4814b2e17b7e74244969
Avijit Dutta <freefirestaravijitdutta@gmail.com>
2026-06-20 10:20:40 +0530

    Add files via upload

---
+++ domain_extanction_for_jjk.py
@@ -0,0 +1,8 @@
+def domain(name_of_the_user,domain_name):
+    print(f"{name_of_the_user}'s domain is ")
+    print(f"domain extanction {domain_name}")
+    print("")
+
+domain("Gojo","borukorsu")
+domain("Mahito","thee ha entos ka")
+domain("Sukuna","maloveran shine")
